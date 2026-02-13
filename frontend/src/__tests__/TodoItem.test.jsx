65948d8c73c6d9dcd4ac0698b72717a21c017b07
siraphat-ohm <siraphat975@gmail.com>
2026-02-13 14:47:28 +0700

    [test] TTD

--- frontend/src/__tests__/TodoItem.test.jsx
+++ frontend/src/__tests__/TodoItem.test.jsx
@@ -1,36 +1,55 @@
 import { render, screen } from '@testing-library/react'
-import { expect } from 'vitest'
+import { assert, expect } from 'vitest'
 import TodoItem from '../components/TodoItem.jsx';
 
 const baseTodo = {             // ** TodoItem พื้นฐานสำหรับทดสอบ
-  id: 1,
-  title: 'Sample Todo',
-  done: false,
-  comments: [],
+    id: 1,
+    title: 'Sample Todo',
+    done: false,
+    comments: [],
 };
 
 describe('TodoItem', () => {
-  it('renders with no comments correctly', () => {    
-    // *** โค้ดสำหรับเทสที่เพิ่มเข้ามา
-    render(
-      <TodoItem todo={baseTodo} />
-    );
-    expect(screen.getByText('Sample Todo')).toBeInTheDocument();
-  });
+    it('renders with no comments correctly', () => {
+        render(
+            <TodoItem todo={baseTodo} />
+        );
+        expect(screen.getByText('Sample Todo')).toBeInTheDocument();
+        if (baseTodo.comments.length === 0) {
+            // add text no comments when there is no comment
+            expect(screen.getByText('No comments')).toBeInTheDocument();
+        }
+    });
 });
 
-  it('renders with comments correctly', () => {
+it('renders with comments correctly', () => {
     const todoWithComment = {
-      ...baseTodo,
-      comments: [
-        {id: 1, message: 'First comment'},
-        {id: 2, message: 'Another comment'},
-      ]
+        ...baseTodo,
+        comments: [
+            { id: 1, message: 'First comment' },
+            { id: 2, message: 'Another comment' },
+        ]
     };
     render(
-      <TodoItem todo={todoWithComment} />
+        <TodoItem todo={todoWithComment} />
     );
+
     expect(screen.getByText('Sample Todo')).toBeInTheDocument();
     expect(screen.getByText('First comment')).toBeInTheDocument();
     expect(screen.getByText('Another comment')).toBeInTheDocument();
-  });
+    expect(screen.getByText(/2/)).toBeInTheDocument();
+});
+
+
+it('does not show no comments message when it has a comment', () => {
+    const todoWithComment = {
+        ...baseTodo,
+        comments: [
+            { id: 1, message: 'First comment' },
+        ]
+    };
+    render(
+        <TodoItem todo={todoWithComment} />
+    );
+    expect(screen.queryByText('No comments')).not.toBeInTheDocument();
+});
