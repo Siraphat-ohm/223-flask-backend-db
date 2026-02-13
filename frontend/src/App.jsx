f70b4d43d83a504984b82179c33d48fdc1953859
siraphat-ohm <siraphat975@gmail.com>
2026-02-13 14:57:27 +0700

    [test] init app test

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,5 +1,5 @@
 import { useState, useEffect } from "react";
-import { TodoItem } from "./components/TodoItem";
+import TodoItem  from "./components/TodoItem";
 import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
 import "./App.css";
