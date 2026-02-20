c725a719f7eebdae39d7e7b382800350e5d9159f
siraphat-ohm <siraphat975@gmail.com>
2026-02-20 14:36:11 +0700

    feat: Login and basic auth

---
+++ frontend/src/components/LoginForm.jsx
@@ -0,0 +1,50 @@
+import { useState } from 'react'
+import { useAuth } from "../context/AuthContext";
+import '../App.css'
+
+function LoginForm({ loginUrl }) {
+    const [username, setUsername] = useState("");
+    const [password, setPassword] = useState("");
+    const { login, username: loggedInUsername } = useAuth();
+
+    const [errorMessage, setErrorMessage] = useState("");
+    async function handleLogin(e) {
+        e.preventDefault();
+        try {
+            const response = await fetch(loginUrl, {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json',
+                },
+                body: JSON.stringify({ username: username, password: password }),
+            });
+            if (response.ok) {
+                const data = await response.json();
+                console.log(data);
+                login(username, data.access_token);
+                alert("Login successful.  access token = " + data.access_token);
+            } else if (response.status === 401) {
+                setErrorMessage("Invalid username or password");
+            }
+        } catch (error) {
+            console.log("Error logging in:", error);
+        }
+    }
+
+
+    return (
+        <form onSubmit={(e) => { handleLogin(e) }}>
+            {errorMessage && <p>{errorMessage}</p>}
+            Username:
+            <input type="text" value={username} onChange={(e) => setUsername(e.target.value)} />
+            <br />
+            Password:
+            <input type="password" value={password} onChange={(e) => setPassword(e.target.value)} />
+            <br />
+            <button type="submit">Login</button>
+            {loggedInUsername && <p>User {loggedInUsername} is already logged in.</p>}
+        </form>
+    );
+}
+
+export default LoginForm;
